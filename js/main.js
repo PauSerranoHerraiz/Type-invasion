b37d4a991f7a0fca1f30cab222e9e04d4c619f49
Pau Serrano <pauserrano14@gmail.com>
2025-11-24 23:42:11 +0100

    feat: random words falling thru the screen

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
-const words = ["hello", "world", "javascript", "mesa", "casa", "luz", "sol"]
-const typedWord = document.getElementById("word");
-let typed = ""
+class Word {
+    constructor() {
+        this.text = this.getRandomWord()
+        this.positionX = Math.floor (Math.random() * 95)
+        this.positionY = 0
+        this.domElement = null
+        this.speed = 0.2 + (Math.random() * 0.3)
+        this.createDomElement()
+        this.updateUi()
+    }
+    createDomElement() {
+        this.domElement = document.createElement("div")
+        this.domElement.className = "word"
+        this.domElement.innerText = this.text;
+        const parentElm = document.getElementById("board")
+        parentElm.appendChild(this.domElement)
+    }
+    updateUi() {
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.top = this.positionY + "vh";
+    }
+    moveDown() {
+        this.positionY += this.speed;
+        this.updateUi()
+    }
+    getRandomWord() {
+        const words = ["hello", "world", "table", "sea", "home", "sun", "pen", "cry", "beef", "quiet", "shock", "order", 
+            "bubble", "beetle", "basketball", "rule"]
+        return words[Math.floor(Math.random() * words.length)]
+    }
 
-function randomWord() {
-    const newWord = words[Math.floor(Math.random() * words.length)]
-    typedWord.textContent = newWord;
-    typedWord.classList.remove("hidden");
-    typed = "";
 }
 
-setInterval(randomWord, 5000)
-randomWord();
+
+const typedWord = document.getElementById("user-input");
+// console.log(typedWord.value);
+
+
+
+const wordInstancesArr = []
+
+// generate new words
+setInterval(() => {
+    const newWordInstance = new Word()
+    wordInstancesArr.push(newWordInstance)
+}, 5000)
+
+setInterval(() => {
+    wordInstancesArr.forEach((element, i, arr) => {
+        element.moveDown()
+    })
+}, 40)
+
+
 
 
 document.addEventListener("keydown", (e) => {
@@ -19,17 +60,10 @@ document.addEventListener("keydown", (e) => {
     if (typed === typedWord.textContent.toLowerCase()) {
         typedWord.classList.add("hidden")
         typed = ""
-        setTimeout (randomWord, 300)
+        setTimeout(getRandomWord, 300)
     }
 })
-/*
-const input = document.getElementById("word")
 
-input.addEventListener("input", function () {
-    if(input.value === newWord) {
-    words.style.display = "none"
-}
-}) */
 
 
 
